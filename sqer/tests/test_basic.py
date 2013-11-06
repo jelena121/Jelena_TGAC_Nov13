85a3d0a1719cb5dfd4f01c1b5d9af6090688bcfa
Jelena Aleksic <jelena121@gmail.com>
2013-11-06 11:22:12 +0000

    added tests and updated gitignore

---
+++ sqer/tests/test_basic.py
@@ -0,0 +1,14 @@
+import sqer
+
+class FakeRecord(object):
+    def __init__(self, sequence, name=''):
+        self.sequence = sequence
+        self.name = name
+
+def test_sum_bp():
+    r = FakeRecord('ATGC')
+    assert sqer.sum_bp(r) == 4
+
+def test_sum_bp_records():
+    rl = [ FakeRecord("A"), FakeRecord("G") ]
+    assert sqer.sum_bp_records(rl) == 2
